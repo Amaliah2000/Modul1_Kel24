3396c1970635f5c219bc5290e5423378ebc701db
ireister <johanesaldrian@gmail.com>
2021-09-09 06:37:32 +0700

    Fix
    fix

--- Tugas_Modul1_Kel24/script.js
+++ Tugas_Modul1_Kel24/script.js
@@ -1,22 +1,10 @@
-var a = 0;
-var b = 0;
-var q2 = ()=> {
-     
-    a = 10;
-    b = 3;
-    console.log(a - b );
+const kelulusan = () =>{
+    var a = document.getElementById("nilaiA").value;
+    var b = document.getElementById("nilaiB").value;
+    var c =  1 * a + 1 * b ;
+    hasil =     c >=50 ? "Pinter": 
+                c <50 ? "Tolol":
+    console.log(hasil); 
+        console.log(c);
+        alert(hasil);
 }
-q2();
-
-console.log(a + b);
-
-(a=>10) ? console.log("yay") : console.log("nooo");
-
-var angka = [2, 3, 5];
-console.log(angka);
-;
-
-const myArray = [5,10,20]
-const reducer = myArray.reduce((total, jumlah)=>total + jumlah)
-
-console.log(reducer);
